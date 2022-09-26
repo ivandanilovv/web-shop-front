234f5f84bfc7a12d6f46b5188ad399e358b1187e
Ivan <ivandanilov282@gmail.com>
2022-09-26 15:55:44 +0200

    Init with axios call

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="fetchSomething">click</button>
+    {{ip.data}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      ip: null,
+    }
+  },
+  methods: {
+    async fetchSomething() {
+      this.ip = await this.$axios.$get('http://127.0.0.1:8000/api/products')
+    }
+  }
+}
+</script>
