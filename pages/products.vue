f1951a687b549fc15a66f749ddda7cc7009543f4
Ivan <ivandanilov282@gmail.com>
2022-09-28 23:34:26 +0200

    Changed design, pagination for products

--- pages/products.vue
+++ pages/products.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
     <custom-navbar active-page="products"/>
-    <div class="row">
-      <div class="col-5 d-flex align-items-start justify-content-start mt-5">
-        <p class="mt-1">
+    <div class="row pb-5">
+      <div class="col-5">
+        <div class="d-flex flex-row my-3">
+        <p class="m-0 ms-3">
           View products for:
         </p>
         <select class="form-control w-30 ms-3" id="selectCategory" v-model="key">
@@ -14,8 +15,28 @@
         <button class="btn btn-primary ms-3" @click="getCategoryId">
           Filter products
         </button>
+        </div>
+        <div class="d-flex justify-content-center align-items-center" v-if="product">
+          <div class="card rounded-2">
+<!--            <img :src="'http://127.0.0.1:5173/public/images/' + product.data.image"-->
+<!--                 class="img-fluid rounded-top" alt="test">-->
+            <img src="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"
+                 class="img-fluid rounded-top" alt="Image" height="50">
+            <div class="card-body">
+              <h5 class="card-title">
+                {{product.data.name}}
+              </h5>
+              <p class="card-text">
+                {{product.data.description}}
+              </p>
+              <p>
+                Price: <span class="text-primary">{{product.data.price}}&euro;</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-7 d-flex align-items-center justify-content-center" v-if="searchCategory">
+      <div class="col-7 d-flex flex-column align-items-center justify-content-center" v-if="searchCategory">
         <table class="table table-striped table-hover table-responsive">
           <thead>
           <tr>
@@ -38,22 +59,13 @@
           </tr>
           </tbody>
         </table>
-      </div>
-    </div>
-    <div class="d-flex justify-content-center align-items-center" v-if="product">
-      <div class="card w-30 rounded-4">
-        <img :src="'http://127.0.0.1:5173/public/images/' + product.data.image"
-             class="img-fluid rounded-top" alt="test">
-        <div class="card-body">
-          <h5 class="card-title">
-            {{product.data.name}}
-          </h5>
-          <p class="card-text">
-            {{product.data.description}}
-          </p>
-          <p>
-            Price: <span class="text-primary">{{product.data.price}}&euro;</span>
-          </p>
+        <div>
+          <button type="button" :disabled="!products.links.prev" @click="getPage(products.links.prev)">
+            Previous
+          </button>
+          <button type="button" :disabled="!products.links.next" @click="getPage(products.links.next)">
+            Next
+          </button>
         </div>
       </div>
     </div>
@@ -78,6 +90,8 @@ export default {
       searchCategory: '',
       searchCategoryId: null,
       product: null,
+      allImages: null,
+      page: 0,
     }
   },
   created() {
@@ -85,11 +99,15 @@ export default {
     this.getProducts();
   },
   methods: {
+    async getPage(link) {
+      if(link!==null)
+        this.products = await this.$axios.$get(link);
+    },
     async getCategories() {
       this.categories = await this.$axios.$get('http://127.0.0.1:8000/api/categories')
     },
     async getProducts() {
-      this.products = await this.$axios.$get('http://127.0.0.1:8000/api/products')
+      this.products = await this.$axios.$get('http://127.0.0.1:8000/api/products?page=1')
     },
     async getProduct(id) {
       this.product = (await this.$axios.$get('http://127.0.0.1:8000/api/products/' + id))
@@ -103,6 +121,9 @@ export default {
         }
       }
     },
+    getImages(item) {
+      this.allImages = item.split(", ");
+    }
   }
 }
 </script>
