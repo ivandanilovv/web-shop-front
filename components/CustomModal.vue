94c9cadd32cb916182e7300ab12112c429bbd349
Ivan <ivandanilov282@gmail.com>
2022-09-30 19:17:44 +0200

    Created custom modal component

---
+++ components/CustomModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal-body">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item ratio ratio-16x9" v-for="(item, i) in allImages" :class="{'active' : i===0}"
+             v-if="i!==allImages.length-1">
+          <img :src="'http://127.0.0.1:5173/public/images/' + item"
+               class="rounded-top" alt="Image">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+              data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+              data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <h2 class="mt-3 fw-bold text-primary">
+      {{ product.data.name }}
+    </h2>
+    <p class="fs-5">
+      {{ product.data.description }}
+    </p>
+    <p class="fs-4">
+      Price: <span class="text-primary fw-bold">{{ product.data.price }}&euro;</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomModal",
+  props: {
+    allImages: Array,
+    product: Object,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/products.vue
+++ pages/products.vue
@@ -11,36 +11,7 @@
             </h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item ratio ratio-16x9" v-for="(item, i) in allImages" :class="{'active' : i===0}"
-                     v-if="i!==allImages.length-1">
-                  <img :src="'http://127.0.0.1:5173/public/images/' + item"
-                       class="rounded-top" alt="Image">
-                </div>
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                      data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                      data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
-            <h2 class="mt-3 fw-bold text-primary">
-              {{ product.data.name }}
-            </h2>
-            <p class="fs-5">
-              {{ product.data.description }}
-            </p>
-            <p class="fs-4">
-              Price: <span class="text-primary fw-bold">{{ product.data.price }}&euro;</span>
-            </p>
-          </div>
+          <custom-modal :all-images="allImages" :product="product"/>
           <div class="modal-footer">
             <button type="button" class="btn btn-primary" @click="addToFavourites(product.data)">
               Add to favourites
@@ -110,10 +81,12 @@
 <script>
 import Favourites from "@/components/Favourites";
 import CustomNavbar from "@/components/CustomNavbar";
+import CustomModal from "@/components/CustomModal";
 
 export default {
   name: "products",
   components: {
+    CustomModal,
     Favourites,
     CustomNavbar
   },
